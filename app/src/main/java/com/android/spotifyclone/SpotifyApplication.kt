aa183d17f608d23a35dbb47329024b9a14273547
Anish <anishanand3733@gmail.com>
2020-09-23 00:01:31 +0530

    Dagger hilt setup

---
+++ app/src/main/java/com/android/spotifyclone/SpotifyApplication.kt
@@ -0,0 +1,7 @@
+package com.android.spotifyclone
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SpotifyApplication: Application()
--- app/src/test/java/com/android/spotifyclone/ExampleUnitTest.kt
+++ app/src/test/java/com/android/spotifyclone/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.plcoding.spotifycloneyt
+package com.android.spotifyclone
 
 import org.junit.Test
 
