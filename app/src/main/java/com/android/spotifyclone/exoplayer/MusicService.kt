6cb4bbae2c562d9f21ac28cbe87afeedc9cf02ad
Anish <anishanand3733@gmail.com>
2020-10-28 21:37:45 +0530

    Finish music service

--- app/src/main/java/com/android/spotifyclone/exoplayer/MusicService.kt
+++ app/src/main/java/com/android/spotifyclone/exoplayer/MusicService.kt
@@ -1,7 +1,9 @@
 package com.android.spotifyclone.exoplayer
 
 import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Browser
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaDescriptionCompat
 import android.support.v4.media.MediaMetadataCompat
@@ -10,6 +12,7 @@ import androidx.media.MediaBrowserServiceCompat
 import com.android.spotifyclone.exoplayer.callbacks.MusicPlaybackPreparer
 import com.android.spotifyclone.exoplayer.callbacks.MusicPlayerEventListener
 import com.android.spotifyclone.exoplayer.callbacks.MusicPlayerNotificationListener
+import com.android.spotifyclone.other.Constants.MEDIA_ROOT_ID
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
@@ -45,6 +48,15 @@ class MusicService : MediaBrowserServiceCompat() {
 
     private var curPlayingSong: MediaMetadataCompat? = null
 
+    private var isPlayerInitialized = false
+
+    private lateinit var musicPlayerEventListener: MusicPlayerEventListener
+
+    companion object {
+        var curSongDuration = 0L
+            private set
+    }
+
     override fun onCreate() {
         super.onCreate()
         serviceScope.launch {
@@ -67,7 +79,7 @@ class MusicService : MediaBrowserServiceCompat() {
             mediaSession.sessionToken,
             MusicPlayerNotificationListener(this)
         ) {
-
+            curSongDuration = exoPlayer.duration
         }
 
         val musicPlaybackPreparer = MusicPlaybackPreparer(firebaseMusicSource) {
@@ -81,9 +93,11 @@ class MusicService : MediaBrowserServiceCompat() {
 
         mediaSessionConnector = MediaSessionConnector(mediaSession)
         mediaSessionConnector.setPlaybackPreparer(musicPlaybackPreparer)
+        mediaSessionConnector.setQueueNavigator(MusicQueueNavigator())
         mediaSessionConnector.setPlayer(exoPlayer)
 
-        exoPlayer.addListener(MusicPlayerEventListener(this))
+        musicPlayerEventListener = MusicPlayerEventListener(this)
+        exoPlayer.addListener(musicPlayerEventListener)
         musicNotificationManager.showNotification(exoPlayer)
     }
 
@@ -104,9 +118,17 @@ class MusicService : MediaBrowserServiceCompat() {
         exoPlayer.playWhenReady = playNow
     }
 
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        super.onTaskRemoved(rootIntent)
+        exoPlayer.stop()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         serviceScope.cancel()
+
+        exoPlayer.removeListener(musicPlayerEventListener)
+        exoPlayer.release()
     }
 
     override fun onGetRoot(
@@ -114,13 +136,30 @@ class MusicService : MediaBrowserServiceCompat() {
         clientUid: Int,
         rootHints: Bundle?
     ): BrowserRoot? {
-        TODO()
+        return BrowserRoot(MEDIA_ROOT_ID, null)
     }
 
     override fun onLoadChildren(
         parentId: String,
         result: Result<MutableList<MediaBrowserCompat.MediaItem>>
     ) {
-
+        when(parentId) {
+            MEDIA_ROOT_ID -> {
+                val resultSent = firebaseMusicSource.whenReady { isInitialized ->
+                    if(isInitialized) {
+                        result.sendResult(firebaseMusicSource.asMediaItems())
+                        if(!isPlayerInitialized && firebaseMusicSource.songs.isNotEmpty()) {
+                            preparePlayer(firebaseMusicSource.songs, firebaseMusicSource.songs[0], false)
+                            isPlayerInitialized = true
+                        }
+                    } else {
+                        result.sendResult(null)
+                    }
+                }
+                if(!resultSent) {
+                    result.detach()
+                }
+            }
+        }
     }
 }
